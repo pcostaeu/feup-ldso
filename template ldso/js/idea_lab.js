f3d93b7f092bde5575726569aa151c81d1513552
Paulo Costa <pajocos@gmail.com>
2015-10-29 00:05:45 +0000

    bug solved

--- template ldso/js/idea_lab.js
+++ template ldso/js/idea_lab.js
@@ -47,13 +47,13 @@ function counters() {
 }
 
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('#click').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: ($("#submitIdea").position().top - 200)
+          scrollTop: ($("#submitIdea").position().top - 140)
         }, 1000);
         return false;
       }
