6dde95123ad3e3e5bc81f3752aefbf305dd4f71f
Luis Pinto <lsmoreira_77@hotmail.com>
2015-11-03 13:46:25 +0000

    codigo do counter movido para file counter.js

--- app/assets/javascripts/idea_lab.js
+++ app/assets/javascripts/idea_lab.js
@@ -21,31 +21,6 @@ $("#nav").on("affix-top.bs.affix", function() {
 });
 });
 
-function startCounters () {
-	values = counters();
-
-	$('.counter1').counter({initial: values[0], clockNum: 0});
-	$('.counter2').counter({initial: values[1], clockNum: 1});
-	$('.counter3').counter({initial: values[2], clockNum: 2});
-}
-
-function counters() {
-	var datainicio = new Date(2015, 0, 1, 0, 0, 0, 0);
-	var datafim = new Date();
-	var tempo = Math.floor((datafim - datainicio) / 1000);
-
-	var electricidade_consumo = (0.215 * tempo).toFixed(2);
-	var agua_consumo = (0.001367 * tempo).toFixed(2);
-	var papel_consumo = Math.floor(0.12 * tempo);
-
-	var values = new Object();
-	values[0] = Math.round(electricidade_consumo);
-	values[1] = Math.round(agua_consumo);
-	values[2] = Math.round(papel_consumo);
-
-	return values;
-}
-
 $(function() {
   $('#click').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
