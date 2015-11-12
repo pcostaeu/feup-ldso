4faa5f4a99759572b7ed6568c8beb83bf03dab8f
Paulo Costa <pajocos@gmail.com>
2015-11-12 11:58:07 +0000

    corrected view of idea after submit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,10 @@
 class IdeasController < ApplicationController
   def index
-  if params[:search] && !params[:search].blank?
-    @ideas = Idea.search(params[:search]).order("created_at DESC")
-  else
-    @ideas = Idea.where(approved: true).order('created_at DESC')
-  end
+    if params[:search] && !params[:search].blank?
+      @ideas = Idea.search(params[:search]).order('created_at DESC')
+    else
+      @ideas = Idea.where(approved: true).order('created_at DESC')
+    end
   end
 
   def show
@@ -13,28 +13,38 @@ class IdeasController < ApplicationController
 
   def new
   end
+
   def upvote
-        @idea = Idea.find(params[:id])
-        @idea.increment!(:upvotes)
-        redirect_to :back
+    @idea = Idea.find(params[:id])
+    @idea.increment!(:upvotes)
+    redirect_to :back
   end
 
-    def downvote
-        @idea = Idea.find(params[:id])
-        @idea.increment!(:downvotes)
-        redirect_to :back
-end
+  def downvote
+    @idea = Idea.find(params[:id])
+    @idea.increment!(:downvotes)
+    redirect_to :back
+    end
 
   def create
     @idea = Idea.new(idea_params)
-    @idea.date = Time.zone.now.to_date;
-    @idea.approved = false;
+    @idea.date = Time.zone.now.to_date
+    @idea.approved = false
     @idea.save
     redirect_to @idea
   end
 
-private
+  def cancel
+    @idea = Idea.find(params[:id])
+    var = params[:id]
+    @idea.destroy
+    FileUtils.rm_rf(var)
+    redirect_to ideas_path
+  end
+
+  private
+
   def idea_params
-      params.require(:idea).permit(:author, :email, :title, :text, :picture)
+    params.require(:idea).permit(:author, :email, :title, :text, :picture)
   end
 end
