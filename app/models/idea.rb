c5b9a23d4eac75bd261e8337c3fe7417e7ba0738
Luis Pinto <lsmoreira_77@hotmail.com>
2015-11-12 21:48:48 +0000

    bug fixed in idea models

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,8 +2,7 @@ class Idea < ActiveRecord::Base
     belongs_to :topic
     mount_uploader :picture, PictureUploader
 
-    def self.search(search)
-      where("title ILIKE ?", "%#{search}%")
-      where("text ILIKE ?", "% #{search} %")
+    def self.search(search)       
+        where("title ILIKE ? OR text ILIKE ? OR author ILIKE ?", "%#{search}%","%#{search}%","%#{search}%")     
     end
 end
