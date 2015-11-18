d955a3a2b3a0fbf3edd2adf0ba281eb19ba63377
Silmarie <lyra.belacqua4@gmail.com>
2015-11-18 09:43:08 +0000

    Idea topic image done

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,11 +2,11 @@ class Idea < ActiveRecord::Base
     belongs_to :topic
     mount_uploader :picture, PictureUploader
 
-    def self.search(search)       
-        where("title ILIKE ? OR text ILIKE ?", "%#{search}%","%#{search}%")     
+    def self.search(search)
+        where("title ILIKE ? OR text ILIKE ?", "%#{search}%","%#{search}%")
     end
-    
-         def self.search_by_month(month)
+
+    def self.search_by_month(month)
         where('extract(month from created_at) = ? ', month)
     end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
   has_many :ideas
+  mount_uploader :picture, TopicPictureUploader
 end
