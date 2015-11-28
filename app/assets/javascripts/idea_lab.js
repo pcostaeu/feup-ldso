2e44f913446e482727d559cfeab4a071f8f2be3d
Paulo Costa <pajocos@gmail.com>
2015-11-28 23:43:10 +0000

    some bugs corrected
    Link to ideas on carousel
    New layout to carousel
    Contacts is now responsive as it should be
    order buttons responsive

--- app/assets/javascripts/idea_lab.js
+++ app/assets/javascripts/idea_lab.js
@@ -37,32 +37,34 @@ $(function() {
 });
 
 function hide_when_upvote(id) {
-//esconder botoes
-document.getElementById('voteButtons'+id).style.visibility = "hidden";
-//atualizar numero de votos automaticamente
-var upvotes = document.getElementById('upvote'+id).innerHTML;
-upvotes = upvotes.substring(1, upvotes.length);
-upvotes = Number(upvotes);
-upvotes = upvotes + 1;
-document.getElementById('upvote'+id).innerHTML = "+"+upvotes;
+  //esconder botoes
+  document.getElementById('voteButtons' + id).style.visibility = "hidden";
+  //atualizar numero de votos automaticamente
+  var upvotes = document.getElementById('upvote' + id).innerHTML;
+  upvotes = upvotes.substring(1, upvotes.length);
+  upvotes = Number(upvotes);
+  upvotes = upvotes + 1;
+  document.getElementById('upvote' + id).innerHTML = "+" + upvotes;
 
 }
+
 function hide_when_downvote(id) {
-//esconder botoes
-document.getElementById('voteButtons'+id).style.visibility = "hidden";
-//atualizar numero de votos automaticamente
+  //esconder botoes
+  document.getElementById('voteButtons' + id).style.visibility = "hidden";
+  //atualizar numero de votos automaticamente
 
-var downvotes = document.getElementById('downvote'+id).innerHTML;
-downvotes = downvotes.substring(1, downvotes.length);
-downvotes = Number(downvotes);
-downvotes = downvotes + 1;
-document.getElementById('downvote'+id).innerHTML = "-"+downvotes;
+  var downvotes = document.getElementById('downvote' + id).innerHTML;
+  downvotes = downvotes.substring(1, downvotes.length);
+  downvotes = Number(downvotes);
+  downvotes = downvotes + 1;
+  document.getElementById('downvote' + id).innerHTML = "-" + downvotes;
 
 
 }
+
 function hide_approved_idea(id) {
-alert("Ideia Aprovada ");
-document.getElementById('idea'+id).innerHTML = "<h2>Aprovada</h2>"
-document.getElementById('idea'+id).style.color = "green";
+  alert("Ideia Aprovada ");
+  document.getElementById('idea' + id).innerHTML = "<h2>Aprovada</h2>"
+  document.getElementById('idea' + id).style.color = "green";
 
 }
